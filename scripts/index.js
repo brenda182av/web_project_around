7aa3d7dc94827f8597f44ca2a19aec6f0e0cea12
brenda182av <brenda182av@hotmail.com>
2025-08-26 09:17:58 -0600

    Cambios

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,24 @@
-document.getElementById("editbutton").addEventListener("click", function () {
-  document.getElementById("popup").style.display = "block";
+const editButton = document.getElementById("editbutton");
+const popup = document.getElementById("popup");
+const closePopupBtn = document.getElementById("closePopupBtn");
+const submitBtn = document.getElementById("submitBtn");
+const nameInput = document.getElementById("field1");
+const aboutInput = document.getElementById("field2");
+const profileName = document.querySelector(".profile__name");
+const profileAbout = document.querySelector(".profile__about");
+
+editButton.addEventListener("click", function () {
+  popup.style.display = "block";
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
 });
 
-document.getElementById("submitBtn").addEventListener("click", function () {
-  document.getElementById("popup").style.display = "none";
+closePopupBtn.addEventListener("click", function () {
+  popup.style.display = "none";
+});
+
+submitBtn.addEventListener("click", function () {
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = aboutInput.value;
+  popup.style.display = "none";
 });
