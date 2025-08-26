8ac764e054d67d8cacde038a900b7cd14b9b28d5
brenda182av <brenda182av@hotmail.com>
2025-08-25 20:04:16 -0600

    proyecto

---
+++ READMI.md
@@ -0,0 +1,10 @@
+Sprint 7
+
+Tripleten web_project_around
+Nombre del Proyecto
+JavaScript
+
+Descripción
+En este proyecto buscaremos realizar un diseño, programación y acciones con JavaScript básico
+
+Enlace a Github
---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+document.getElementById("editbutton").addEventListener("click", function () {
+  document.getElementById("popup").style.display = "block";
+});
+
+document.getElementById("submitBtn").addEventListener("click", function () {
+  document.getElementById("popup").style.display = "none";
+});
